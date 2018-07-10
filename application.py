3e21e23ceef71c223a088873852f56b89d87d816
Cali-Ann Sara Martin <calimartin1222@gmail.com>
2018-07-10 18:56:47 +0000

    in the middle of similar search suggestions

--- application.py
+++ application.py
@@ -7,19 +7,17 @@ import requests, json
 
 app = Flask(__name__)
 
-# Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set up database
+userLoggedIn = None
+
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-
 
 @app.route("/")
 def index():
@@ -45,32 +43,53 @@ def account():
 
 @app.route("/dashboard", methods=["POST"])
 def login():
+    global userLoggedIn
     uname = request.form.get("uname")
     pword = request.form.get("pword")
 
-    match = db.execute("SELECT * FROM userinfo WHERE uname = :uname AND pword = pword", {"uname": uname, "pword": pword}).fetchone()
+    match = db.execute("SELECT * FROM userinfo WHERE uname = :uname AND pword = :pword", {"uname": uname, "pword": pword}).fetchone()
     if match is None:
         return render_template("error.html", message="Wrong username or password")
-
-    return render_template("dash.html", user=uname)
+    userLoggedIn=uname
+    return render_template("dash.html", userLoggedIn=userLoggedIn)
 
 @app.route("/logout")
 def logout():
-    return render_template("logout.html")
+    global userLoggedIn
+    return render_template("alert.html", message = " Thank you for visiting, " + userLoggedIn + "!")
+    userLoggedIn = ""
 
-
-@app.route("/weather", methods=["POST"])
-def weather():
-    location = request.form.get("location")
-    match = db.execute("SELECT id FROM locations WHERE city = :loc OR zip = :loc", {"loc": location}).fetchall()
-    if match is None:
+@app.route("/search/results", methods=["POST"])
+def results():
+    matches=[]
+    location = request.form.get("location").upper()
+    matches = db.execute("SELECT city FROM locations WHERE city = :loc OR zip = :loc;", {"loc": location}).fetchall()
+    if not matches:
+        matches = db.execute("SELECT DISTINCT(city) FROM locations WHERE city LIKE '%loc%' OR zip LIKE '%loc%';", {"loc": location}).fetchall()
+        for match in matches:
+            matches.append(match[2:(len(match)-1)])
+    if not matches:
         return render_template("error.html", message="Not a valid city or zipcode")
+    if len(matches) > 1:
+        return render_template("results.html", matches=matches, userLoggedIn=userLoggedIn)
+    else:
+        return render_template("weather.html", city=location)
+
+
+@app.route("/weather", methods=["GET"])
+def weather():
+    location=location
     lat = str(db.execute("SELECT lat FROM locations WHERE city = :loc OR zip = :loc", {"loc": location}).fetchone())
     longi = str(db.execute("SELECT long FROM locations WHERE city = :loc OR zip = :loc", {"loc": location}).fetchone())
-    url = "https://api.darksky.net/forecast/ec674134bed7c60466462a9b3adbaa66/" + lat[2:6] + "," + longi[2:6]
+    if lat[2] == "-":
+        lat = lat[2:8]
+    else:
+        lat = lat[2:7]
+    if longi[2] == "-":
+        longi = longi[2:8]
+    else:
+        longi = longi[2:7]
+    url = "https://api.darksky.net/forecast/ec674134bed7c60466462a9b3adbaa66/" + lat + "," + longi
     #weather = requests.get(url).json()
     weatherInfo = url#json.dumps(weather["currently"], indent = 2)
-    #weather = requests.get("https://api.darksky.net/forecast/ec674134bed7c60466462a9b3adbaa66/42.37,-71.11").json()
-    #weatherInfo = (json.dumps(weather["currently"], indent = 2))
-
     return render_template("weather.html", location=location, weatherInfo=weatherInfo)
