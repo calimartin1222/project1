ef409a8186e7e6a6c1342ce990bbf8681698e6e7
Cali-Ann Sara Martin <calimartin1222@gmail.com>
2018-07-11 22:07:43 +0000

    added checkin features

--- application.py
+++ application.py
@@ -15,12 +15,14 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 userLoggedIn = None
-
+currLocationID = None
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+    userLoggedIn = ""
+    currLocationID=""
     return render_template("index.html")
 
 @app.route("/register")
@@ -58,55 +60,39 @@ def logout():
     global userLoggedIn
     return render_template("alert.html", message = " Thank you for visiting, " + userLoggedIn + "!")
     userLoggedIn = ""
+    currLocationID=""
 
 @app.route("/search/results", methods=["POST"])
 def results():
     global userLoggedIn
     location = request.form.get("location").upper()
     matches=[]
-    matches = db.execute("SELECT city FROM locations WHERE zip LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
-    if len(matches) != 0:
-        matches = db.execute("SELECT state FROM locations WHERE zip LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
-        matchesZip = db.execute("SELECT zip FROM locations WHERE zip LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
+    matches = db.execute("SELECT city, state, zip FROM locations WHERE zip LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
     if len(matches) == 0:
-        matches = db.execute("SELECT city FROM locations WHERE city LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
-        if len(matches) != 0:
-            stateMatches=db.execute("SELECT state FROM locations WHERE city LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
-            matchesZip = db.execute("SELECT zip FROM locations WHERE city LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
+        matches = db.execute("SELECT city, state, zip FROM locations WHERE zip LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
     if len(matches) == 0:
-        matches = db.execute("SELECT city FROM locations WHERE city = :loc OR zip = :loc;", {"loc": location}).fetchone()
-        if len(matches) != 0:
-            stateMatches=db.execute("SELECT state FROM locations WHERE city = :loc OR zip = :loc;", {"loc": location}).fetchone()
-            matchesZip=db.execute("SELECT zip FROM locations WHERE city = :loc OR zip = :loc;", {"loc": location}).fetchone()
+        matches = db.execute("SELECT city, state, zip FROM locations WHERE city LIKE ('%' || :loc || '%');", {'loc': location}).fetchall()
+    if len(matches) == 0:
+        matches = db.execute("SELECT city, state, zip FROM locations WHERE city = :loc OR zip = :loc;", {"loc": location}).fetchone()
     if len(matches) == 0:
         return render_template("error.html", message="Not a valid city or zipcode")
-
-    matchesZipSend = []
-    for match in matchesZip:
-        strMatch = str(match)
-        matchesZipSend.append(strMatch[2:(len(strMatch))-3])
-
-    matchesStateSend = []
-    for match in stateMatches:
-        strMatch = str(match)
-        matchesStateSend.append(strMatch[2:(len(strMatch))-3])
 
     matchesSend = []
     for match in matches:
         currIndex = matches.index(match)
         strMatch = str(match)
-        matchesSend.append(strMatch[2:(len(strMatch))-3] + ", " + matchesStateSend[currIndex] + ", " + matchesZipSend[currIndex])
-
-
-
+        matchesSend.append(strMatch[2:(len(strMatch))-2])
 
     return render_template("results.html", matches=matchesSend, userLoggedIn=userLoggedIn)
 
 @app.route("/search/weather/<string:location>", methods=["GET"])
 def weather(location):
-    location = location.split(",")[0]
+    global userLoggedIn
+    global currLocationID
+    location = location.split("'")[0]
     lat = str(db.execute("SELECT lat FROM locations WHERE city = :loc OR zip = :loc", {"loc": location}).fetchone())
     longi = str(db.execute("SELECT long FROM locations WHERE city = :loc OR zip = :loc", {"loc": location}).fetchone())
+
     if lat[2] == "-":
         lat = lat[2:8]
     else:
@@ -116,6 +102,20 @@ def weather(location):
     else:
         longi = longi[2:7]
     url = "https://api.darksky.net/forecast/ec674134bed7c60466462a9b3adbaa66/" + lat + "," + longi
+    currLocationID =str(db.execute("SELECT id FROM locations WHERE lat = :lat AND long = :longi", {"lat": lat, "longi": longi}).fetchone())
+    currLocationID = currLocationID[1:len(currLocationID)-2]
     weather = requests.get(url).json()
     weatherInfo = json.dumps(weather["currently"], indent = 2)
-    return render_template("weather.html", location=location, weatherInfo=weatherInfo)
+    return render_template("weather.html", location=location, weatherInfo=weatherInfo, userLoggedIn=userLoggedIn)
+@app.route("/search/weather/check-in/", methods=["POST"])
+def checkIn():
+    global userLoggedIn
+    global currLocationID
+    comment = request.form.get("comment")
+    check = db.execute("SELECT id FROM checkins WHERE location = :location OR username = :username;", {"location": currLocationID, "username": userLoggedIn}).fetchone()
+    if not check:
+        db.execute("INSERT INTO checkins (location, username, comment) VALUES (:x, :y, :z)", {"x": currLocationID, "y": userLoggedIn, "z": comment})
+        db.commit()
+    else:
+        return render_template("error.html", message="You've already made a comment about this location")
+    return render_template("alert.html", message="Your comment " + comment + " was added!")
